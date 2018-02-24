daeb873793b206897883fb9253e73d0f18dd49c5
Dimitar Dimitrov <dydimitrov86@gmail.com>
2018-02-24 16:49:26 +0200

    Added P11_PokemonTrainer

---
+++ OOPBasicsExDefiningClasses/PokemonTrainer/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P11_PokemonTrainer
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            var command = Console.ReadLine();
+
+            while (command != "Tournament")
+            {
+
+
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ OOPBasicsExDefiningClasses/PokemonTrainer/Trainer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Trainer
+{
+    private string name;
+    private int badges;
+    private List<Pokemon> listOfPokemons;
+
+    public Trainer()
+    {
+        this.Badges = 0;
+        this.ListOfPokemons = new List<Pokemon>();
+    }
+    public Trainer(string name, List<Pokemon> listOfPokemons)
+        :this()
+    {
+        this.Name = name;
+        this.ListOfPokemons = listOfPokemons;
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }   
+    }
+
+    public int Badges
+    {
+        get { return badges; }
+        set { badges = value; }
+    }
+
+    public List<Pokemon> ListOfPokemons
+    {
+        get { return listOfPokemons; }
+        set { listOfPokemons = value; }
+    }
+
+
+}
