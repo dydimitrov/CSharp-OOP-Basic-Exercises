7c3d1c8a481749df3d41e0f58c4d6980ba492569
Dimitar Dimitrov <dydimitrov86@gmail.com>
2018-02-24 17:01:20 +0200

    progress in excersise

--- OOPBasicsExDefiningClasses/PokemonTrainer/StartUp.cs
+++ OOPBasicsExDefiningClasses/PokemonTrainer/StartUp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P11_PokemonTrainer
 {
@@ -8,14 +10,25 @@ namespace P11_PokemonTrainer
         {
 
             var command = Console.ReadLine();
+            var listOfTrainers = new HashSet<Trainer>();
 
             while (command != "Tournament")
             {
+                var line = command.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
 
+                Pokemon curretPokemon = new Pokemon();
+                curretPokemon.Name = line[1];
+                curretPokemon.Element = line[2];
+                curretPokemon.Health = int.Parse(line[3]);
 
+                Trainer currentTrainer = new Trainer();
+                currentTrainer.Name = line[0];
+                currentTrainer.ListOfPokemons.Add(curretPokemon);
 
+                listOfTrainers.Add(currentTrainer);
                 command = Console.ReadLine();
             }
+            Console.ReadKey();
         }
     }
 }
